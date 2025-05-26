a929b5603450c6bff8fc7704b1d8f2d74fa8955b
Rael <santosrafaelpro@gmail.com>
2025-05-26 15:25:24 -0300

    concluido

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-from pytube import YouTube
+from pytubefix import YouTube
+from sys import argv
+import os
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+print("Views: ", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+download_folder = 'videos'
+
+if not os.path.exists(download_folder):
+    os.makedirs(download_folder)
+
+yd.download(output_path=download_folder)
+
+print(f'Download concluído em {download_folder}!')
